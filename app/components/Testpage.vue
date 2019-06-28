e6112d92c93cb3ebaa0950da8693d84b12a39e30
Christopher Nekich <chriswilldev@gmail.com>
2019-06-28 17:04:24 +1000

    fetch measurables in test page

--- app/components/Testpage.vue
+++ app/components/Testpage.vue
@@ -5,8 +5,28 @@
 </template>
 
 <script>
-export default {
+import {normalizeTrackable, normalizedMeasurable} from '~/utils.js'
 
+export default {
+  data() {
+    test: []
+  },
+  methods: {
+    fetchMeasurables(){
+      this.$store.state.database.all('SELECT * FROM trackables', (err, resultSet) => {
+        this.test = []
+        if(err){
+          console.error(err)
+        }else{
+          resultSet.forEach(element => {
+            let normalized = normalizeTrackable(element)
+            this.test.push(normalized)
+          })
+        }
+      })
+    },
+    
+  } //methods
 }
 </script>
 
